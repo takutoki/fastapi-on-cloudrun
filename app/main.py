0e4a7051bd2ead4e0705dc67c7330cc5176d5dea
takutoki <34600200+takutoki@users.noreply.github.com>
2024-05-15 18:32:31 +0900

    add: postメソッド

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
+import logging
 import os
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 @app.get("/")
@@ -13,6 +17,13 @@ def read_root():
 @app.get("/test")
 def read_test():
     return {"Hello": "Test"}
+
+
+@app.post("/test_post")
+async def post_test(request: Request):
+    json_body = await request.json()
+    logger.info(f"Received JSON body: {json_body}")
+    return {"message": "Body logged"}
 
 
 if __name__ == "__main__":
